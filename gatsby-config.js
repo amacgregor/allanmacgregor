6c69d44bffd93844e427b6f525f8a77b55762d08
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-09-03 18:27:05 -0400

    chore(analytics): update GA tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,7 +148,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-11372057-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
