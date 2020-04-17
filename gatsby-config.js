05b211a3d7fb86c285ce5dff64047fb74d42be67
Allan MacGregor <amacgregor@allanmacgregor.com>
2020-04-17 10:55:27 -0400

    finish post and add feed.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -327,7 +327,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
+        whitelist: ['p-4','italic'], // Don't remove this selector
         ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
