f05bfcec5bede697de399ec827602e47f60c54ce
Allan MacGregor <amacgregor@allanmacgregor.com>
2020-04-17 10:56:24 -0400

    The importance of soft-skills when interviewing (#22)
    * fix: add feed.xml
    
    * finish post and add feed.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -236,6 +236,42 @@ module.exports = {
             output: "/rss.xml",
             title: "Allan's Feed",
             match: "^/posts/"
+          },
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }]
+                });
+              });
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: { frontmatter: { layout: { eq: "post" }, draft: { ne: true } } }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/feed.xml",
+            title: "Allan's Feed",
+            match: "^/posts/"
           }
         ]
       }
@@ -291,7 +327,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
+        whitelist: ['p-4','italic'], // Don't remove this selector
         ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
