26c13758ce48fc6dc9aba27138969862249cf9d4
Allan MacGregor <amacgregor@allanmacgregor.com>
2021-01-14 08:06:44 -0500

    Add Favicon and manifest plugin

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import '../../assets/scss/init.scss'
+import favicon from '../../assets/images/favicon.ico'
 
 class Layout extends React.Component {
   render() {
     const { children } = this.props
     return (
       <div className="lg:h-screen bg-white lg:flex lg:overflow-hidden">
-        <Helmet defaultTitle="Blog by John Doe" />
+        <Helmet defaultTitle="Blog by Allan MacGregor">
+           <link rel="icon" href={favicon} />
+        </Helmet>
         {children}
       </div>
     )
